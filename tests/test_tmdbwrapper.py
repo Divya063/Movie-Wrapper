befe35ee5e3942c8974a313729237ee9172c6f6a
Divya <ranidivya063@gmail.com>
2017-10-24 21:48:03 +0530

    fixed TypeError

--- tests/test_tmdbwrapper.py
+++ tests/test_tmdbwrapper.py
@@ -4,25 +4,24 @@ from tmdbwrapper import TV
 
 @fixture
 def tv_keys():
-    # Responsible only for returning the test data
     return ['id', 'origin_country', 'poster_path', 'name',
               'overview', 'popularity', 'backdrop_path',
               'first_air_date', 'vote_count', 'vote_average']
 
 
-@vcr.use_cassette('tests/vcr_cassettes/tv-info.yml')
-		  
-def test_tv_info():
+@vcr.use_cassette('tests/vcr_cassettes/tv-info.yml')		  
+def test_tv_info(tv_keys):
     """Tests an API call to get a TV show's info"""
 
     tv_instance = TV(1396)
     response = tv_instance.info()
 
     assert isinstance(response, dict)
-    assert response['id'] == 1396, "The ID should be in the response"
+    assert response['id'] == 1396 
+    assert set(tv_keys).issubset(response.keys())
    
 @vcr.use_cassette('tests/vcr_cassettes/tv-popular.yml')
-def test_tv_popular():
+def test_tv_popular(tv_keys):
     """Tests an API call to get a popular tv shows"""
 
     response = TV.popular()
